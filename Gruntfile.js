8dbf6918ccb8e43a2e6756b3d50e5e9a132df718
Rui Baltazar <rui@opsmanager.com>
2016-08-31 12:17:12 +0800

    [Hotfix] moved static inside lms

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,165 +5,165 @@ module.exports = function (grunt) {
     require('load-grunt-tasks')(grunt);
 
     var config = {
-        src: 'src',
-        dest: 'static',
-        bower: 'bower_components'
+	src: 'src',
+	dest: 'lms/static',
+	bower: 'bower_components'
     };
 
     grunt.initConfig({
-        c: config,
+	c: config,
 
-        // ## //
+	// ## //
 
-        watch: {
-            less: {
-                files: ['<%= c.src %>/less/**/*.less'],
-                tasks: ['less', 'autoprefixer', 'cssmin', 'bless']
-            }
-        },
+	watch: {
+	    less: {
+		files: ['<%= c.src %>/less/**/*.less'],
+		tasks: ['less', 'autoprefixer', 'cssmin', 'bless']
+	    }
+	},
 
-        // ## //
+	// ## //
 
-        jshint: {
-            node: {
-                options: {
-                    jshintrc: true
-                },
-                files: {
-                    src: [
-                        'Gruntfile.js'
-                    ]
-                }
-            }
-        },
+	jshint: {
+	    node: {
+		options: {
+		    jshintrc: true
+		},
+		files: {
+		    src: [
+			'Gruntfile.js'
+		    ]
+		}
+	    }
+	},
 
-        // ## //
+	// ## //
 
-        jscs: {
-            all: {
-                files: {
-                    src: [
-                        'Gruntfile.js'
-                    ]
-                }
-            }
-        },
+	jscs: {
+	    all: {
+		files: {
+		    src: [
+			'Gruntfile.js'
+		    ]
+		}
+	    }
+	},
 
-        // ## //
+	// ## //
 
-        less: {
-            theme: {
-                options: {
-                    paths: ['<%= c.bower %>']
-                },
-                files: {
-                    '<%= c.dest %>/css/bundle.css': '<%= c.src %>/less/main.less'
-                }
-            }
-        },
+	less: {
+	    theme: {
+		options: {
+		    paths: ['<%= c.bower %>']
+		},
+		files: {
+		    '<%= c.dest %>/css/bundle.css': '<%= c.src %>/less/main.less'
+		}
+	    }
+	},
 
-        // ## //
+	// ## //
 
-        autoprefixer: {
-            theme: {
-                options: {
-                    browsers: ['last 2 versions', 'ie 9']
-                },
-                files: {
-                    '<%= c.dest %>/css/bundle.css': [
-                        '<%= c.dest %>/css/bundle.css'
-                    ]
-                }
-            }
-        },
+	autoprefixer: {
+	    theme: {
+		options: {
+		    browsers: ['last 2 versions', 'ie 9']
+		},
+		files: {
+		    '<%= c.dest %>/css/bundle.css': [
+			'<%= c.dest %>/css/bundle.css'
+		    ]
+		}
+	    }
+	},
 
-        // ## //
+	// ## //
 
-        bless: {
-            theme: {
-                options: {
-                    force: true
-                },
-                files: {
-                    '<%= c.dest %>/css/bundle.css': [
-                        '<%= c.dest %>/css/bundle.css'
-                    ]
-                }
-            }
-        },
+	bless: {
+	    theme: {
+		options: {
+		    force: true
+		},
+		files: {
+		    '<%= c.dest %>/css/bundle.css': [
+			'<%= c.dest %>/css/bundle.css'
+		    ]
+		}
+	    }
+	},
 
-        // ## //
+	// ## //
 
-        cssmin: {
-            theme: {
-                options: {
-                    report: 'min'
-                },
-                files: [{
-                    expand: true,
-                    cwd: '<%= c.dest %>/css',
-                    dest: '<%= c.dest %>/css',
-                    src: ['*.css']
-                }]
-            }
-        },
+	cssmin: {
+	    theme: {
+		options: {
+		    report: 'min'
+		},
+		files: [{
+		    expand: true,
+		    cwd: '<%= c.dest %>/css',
+		    dest: '<%= c.dest %>/css',
+		    src: ['*.css']
+		}]
+	    }
+	},
 
-        // ## //
+	// ## //
 
-        imagemin: {
-            theme: {
-                options: {
-                    report: 'min'
-                },
-                files: [{
-                    expand: true,
-                    cwd: '<%= c.dest %>/images/',
-                    dest: '<%= c.dest %>/images/',
-                    src: [
-                        '**/*.{png,jpg,gif}'
-                    ]
-                }]
-            }
-        },
+	imagemin: {
+	    theme: {
+		options: {
+		    report: 'min'
+		},
+		files: [{
+		    expand: true,
+		    cwd: '<%= c.dest %>/images/',
+		    dest: '<%= c.dest %>/images/',
+		    src: [
+			'**/*.{png,jpg,gif}'
+		    ]
+		}]
+	    }
+	},
 
-        // ## //
+	// ## //
 
-        copy: {
-            fontawesome: {
-                files: [{
-                    expand: true,
-                    flatten: true,
-                    cwd: '<%= c.bower %>/',
-                    dest: '<%= c.dest %>/fonts/font-awesome/',
-                    src: [
-                        'font-awesome/fonts/*'
-                    ]
-                }]
-            }
-        }
+	copy: {
+	    fontawesome: {
+		files: [{
+		    expand: true,
+		    flatten: true,
+		    cwd: '<%= c.bower %>/',
+		    dest: '<%= c.dest %>/fonts/font-awesome/',
+		    src: [
+			'font-awesome/fonts/*'
+		    ]
+		}]
+	    }
+	}
     });
 
     grunt.registerTask('build', [
-        'less',
-        'autoprefixer',
-        'cssmin',
-        'bless',
-        'imagemin',
-        'copy'
+	'less',
+	'autoprefixer',
+	'cssmin',
+	'bless',
+	'imagemin',
+	'copy'
     ]);
 
     grunt.registerTask('default', function () {
-        grunt.option('force', true);
+	grunt.option('force', true);
 
-        grunt.task.run([
-            'build',
-            'watch'
-        ]);
+	grunt.task.run([
+	    'build',
+	    'watch'
+	]);
     });
 
     grunt.registerTask('test', [
-        'jshint',
-        'jscs',
-        'build'
+	'jshint',
+	'jscs',
+	'build'
     ]);
 };
